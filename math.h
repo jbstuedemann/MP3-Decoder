18e3b247b806f678cbbe3b597416c56d50898303
Pranav Eswaran <chesseswar@gmail.com>
2020-11-20 11:21:55 -0600

    cs, ca arrays in tables.h defined sqrt 2 in math.h

--- math.h
+++ math.h
@@ -4,12 +4,14 @@
 #include "stdint.h"
 #include <math.h>
 #undef M_PI
+#undef M_SQRT2
 
 namespace util {
 
     namespace math {
 
         constexpr double M_PI = 3.141592653589793;
+        constexpr double M_SQRT2 = 1.41421356237309505;
 
         inline double power(double a, double b){ return std::pow(a, b); }
 
